bb35e7ac8b9c45df7db93baafa95e72542a9eb2d
Tom Kane <thomas.kane@diamond.ac.uk>
2025-12-17 12:25:36 +0000

    Add gateways service

---
+++ ec-setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# load ec configured for the training beamline P47
+module load ec/p47
+
+# re-configure it to point at your personal namespace
+EC_SERVICES_REPO=git@github.com:tomkane-dls/t01-services.git
+EC_TARGET=kgv66629/t01
+
+# load argus configuration for kubectl
+module load argus
